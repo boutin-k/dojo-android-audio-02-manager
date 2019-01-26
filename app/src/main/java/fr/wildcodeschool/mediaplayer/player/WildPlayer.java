f074f5b99d4a8d0eb854957f457e6a00891f78c8
ken <ken@wildcodeschool.fr>
2019-01-26 16:57:10 +0100

    Clear seekBarHandler and reset WildPlayer isPrepared on activity destroy

--- app/src/main/java/fr/wildcodeschool/mediaplayer/player/WildPlayer.java
+++ app/src/main/java/fr/wildcodeschool/mediaplayer/player/WildPlayer.java
@@ -139,6 +139,7 @@ public class WildPlayer implements WildAudioManagerListener {
     if (null != mPlayer && isPrepared) {
       mPlayer.release();
       WildAudioManager.getInstance().releaseAudioFocus();
+      isPrepared = false;
     }
   }
 
